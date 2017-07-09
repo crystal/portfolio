2e01595d669063a948bd7ea4ae1edeeb8b59d2d9
Crystal Tate <crys.tate@icloud.com>
2017-07-09 00:54:19 -0500

    Styles tech

--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React, { Component } from 'react';
 
 import styles from './Tech.sass';
@@ -11,26 +12,50 @@ class Tech extends Component {
           <ul className={styles.tiles}>
             <li>
               <div className={styles.tile}>
-                <h3>Node.Js</h3>
-                <p>test test test</p>
+                <div className={classNames(styles.image, styles.javascript)} />
+                <h3>JavaScript (ES6/ES7)</h3>
               </div>
             </li>
             <li>
               <div className={styles.tile}>
-                <h3>JavaScript</h3>
-                <p>test test test</p>
-              </div>
-            </li>
-            <li>
-              <div className={styles.tile}>
+                <div className={classNames(styles.image, styles.react)} />
                 <h3>React</h3>
-                <p>test test test</p>
               </div>
             </li>
             <li>
               <div className={styles.tile}>
-                <h3>Forth</h3>
-                <p>test test test</p>
+                <div className={classNames(styles.image, styles.express)} />
+                <h3>ExpressJS</h3>
+              </div>
+            </li>
+            <li>
+              <div className={styles.tile}>
+                <div className={classNames(styles.image, styles.angular)} />
+                <h3>Angular</h3>
+              </div>
+            </li>
+            <li>
+              <div className={styles.tile}>
+                <div className={classNames(styles.image, styles.lambda)} />
+                <h3>AWS Lambda</h3>
+              </div>
+            </li>
+            <li>
+              <div className={styles.tile}>
+                <div className={classNames(styles.image, styles.ec2)} />
+                <h3>AWS EC2</h3>
+              </div>
+            </li>
+            <li>
+              <div className={styles.tile}>
+                <div className={classNames(styles.image, styles.dynamodb)} />
+                <h3>AWS DynamoDB</h3>
+              </div>
+            </li>
+            <li>
+              <div className={styles.tile}>
+                <div className={classNames(styles.image, styles.firebase)} />
+                <h3>Firebase</h3>
               </div>
             </li>
           </ul>
