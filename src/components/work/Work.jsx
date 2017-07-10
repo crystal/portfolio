7665ec7b0e9fd2da3bbd33434a563b0cdea8ffdc
Crystal Tate <crys.tate@icloud.com>
2017-07-10 13:13:36 -0500

    Adds project links and about me content

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -12,22 +12,22 @@ class Work extends Component {
             <li>
               <div className={styles.tile}>
                 <div className={styles.image} style={{ backgroundImage: 'url(images/desk.jpg)' }}/>
-                <h3>Templist</h3>
-                <p>Templist is a Trello Integration made in React. Select to-do list templates and export them to Trello!</p>
+                <h3><a href="http://crystal.github.io/templist" target="_blank" rel="noopener noreferrer">Templist</a></h3>
+                <p>Templist is a Trello Integration made in React using Firebase. Users can pick a to-do list template and export it to Trello!</p>
               </div>
             </li>
             <li>
               <div className={styles.tile}>
                 <div className={styles.image} style={{ backgroundImage: 'url(images/friends.jpg)' }}/>
-                <h3>Midpoint</h3>
-                <p>Midpoint helps friends meet in the middle! Find points of interest - somewhere around the middle.</p>
+                <h3><a href="https://github.com/ht44/midpoint" target="_blank" rel="noopener noreferrer" >Midpoint</a></h3>
+                <p>Midpoint uses the location of a group of friends, calculates the midpoint and find points of interest near the middle.</p>
               </div>
             </li>
             <li>
               <div className={styles.tile}>
                 <div className={styles.image} style={{ backgroundImage: 'url(images/wakeup.jpg)' }}/>
-                <h3>Alexa Skill: A New Day</h3>
-                <p>A New Day guides you through your morning routine to keep you on task and on schedule.</p>
+                <h3><a href="https://github.com/crystal/a-new-day" target="_blank" rel="noopener noreferrer">Alexa Skill: A New Day</a></h3>
+                <p>A New Day is an Alexa app which guides you through your morning routine to keep you on task and on schedule.</p>
               </div>
             </li>
           </ul>
