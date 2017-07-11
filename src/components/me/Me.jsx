5f263466d0f7a88cb403a128f04142716cfbb254
Crystal Tate <crys.tate@icloud.com>
2017-07-10 21:19:51 -0500

    Removes about text

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -9,11 +9,11 @@ class Me extends Component {
         <section>
           <h1>Crystal Tate</h1>
           <h2>Full Stack Software Engineer</h2>
-          <p>I am based in Austin, Texas where I've worked remotely on distributed teams for 10 years.</p>
+          {/* <p>I am based in Austin, Texas where I've worked remotely on distributed teams for 10 years.</p>
           <p>Being a developer allows me to focus on the details, solve problems thoughtfully,
             and learn continuously. I enjoy working with new languages and technologies to expand my toolkit.
             My experience with project management and UX/UI has given me a passion (read: obsession)
-            for pixel-perfect and friction-free front end design.</p>
+            for pixel-perfect and friction-free front end design.</p> */}
         </section>
       </div>
     );
