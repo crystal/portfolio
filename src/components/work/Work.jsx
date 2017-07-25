e8d2b62638e5a28a54db4ab1ac8be81c242235d9
Crystal Tate <crys.tate@icloud.com>
2017-07-25 10:46:15 -0500

    updates work tiles
    - includes project screenshots / image assets
    - updates title and description for TempLists
    - fixes tile background and link colors

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -11,23 +11,23 @@ class Work extends Component {
           <ul className={styles.tiles}>
             <li>
               <div className={styles.tile}>
-                <div className={styles.image} style={{ backgroundImage: 'url(images/desk.jpg)' }}/>
-                <h3><a href="http://crystal.github.io/templist" target="_blank" rel="noopener noreferrer">Templist</a></h3>
-                <p>Templist is a Trello Integration made in React using Firebase. Users can pick a to-do list template and export it to Trello!</p>
+                <div className={styles.image} style={{ backgroundImage: 'url(images/TempLists_img.png)' }}/>
+                <h3><a href="http://templists.com" target="_blank" rel="noopener noreferrer">TempLists</a></h3>
+                <p>TempLists is a Trello Integration made in React using Firebase. Users can pick a to-do list template and export it to Trello!</p>
               </div>
             </li>
             <li>
               <div className={styles.tile}>
-                <div className={styles.image} style={{ backgroundImage: 'url(images/friends.jpg)' }}/>
+                <div className={styles.image} style={{ backgroundImage: 'url(images/Midpoint.png)' }}/>
                 <h3><a href="https://github.com/ht44/midpoint" target="_blank" rel="noopener noreferrer" >Midpoint</a></h3>
                 <p>Midpoint uses the location of a group of friends, calculates the midpoint and find points of interest near the middle.</p>
               </div>
             </li>
             <li>
               <div className={styles.tile}>
-                <div className={styles.image} style={{ backgroundImage: 'url(images/wakeup.jpg)' }}/>
+                <div className={styles.image} style={{ backgroundImage: 'url(images/a-new-day-logo.png)' }}/>
                 <h3><a href="https://github.com/crystal/a-new-day" target="_blank" rel="noopener noreferrer">Alexa Skill: A New Day</a></h3>
-                <p>A New Day is an Alexa app which guides you through your morning routine to keep you on task and on schedule.</p>
+                <p>A New Day is an Alexa Skill which guides you through your morning routine to keep you on task and on schedule.</p>
               </div>
             </li>
           </ul>
