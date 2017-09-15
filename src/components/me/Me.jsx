b4637b3dca7408d8fd248c8b457024946c5c54c6
Crystal Tate <crys.tate@icloud.com>
2017-09-15 11:09:03 -0500

    adds resume via s3

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -9,6 +9,7 @@ class Me extends Component {
         <section>
           <h1>Crystal Tate</h1>
           <h2>Full Stack Software Engineer</h2>
+          <h3><a href="https://s3.amazonaws.com/ctate-resume/Crystal+Tate+Resume.pdf" target="_blank" rel="noopener noreferrer">View My Resume</a></h3>
           {/* <p>I am based in Austin, Texas where I've worked remotely on distributed teams for 10 years.</p>
           <p>Being a developer allows me to focus on the details, solve problems thoughtfully,
             and learn continuously. I enjoy working with new languages and technologies to expand my toolkit.
